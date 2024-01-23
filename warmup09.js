657a72c2f132ea6764f5c1b4f2dc206070562bbb
Ahmedghalleb7 <ghallebahmed070215@gmail.com>
2024-01-23 11:37:34 +0100

    work in progress

--- warmup09.js
+++ warmup09.js
@@ -9,38 +9,48 @@
 // - Write a function searchMates that, given a query and an array of Mates,
 //   searches the array of mates for "matching" mate. You will decide what way you want to write your search algorithm.
 
-let classmates = (name,age,sexe,favecolor,fears,allergi)=>{
+let classmates = (name, age, sexe, favecolor, fears, allergi) => {
     return {
-        name:name,
-        age:age,
-        sexe:sexe,
-        favecolor:favecolor,
-        fears:fears,
-        allergi:allergi
+        name: name,
+        age: age,
+        sexe: sexe,
+        favecolor: favecolor,
+        fears: fears,
+        allergi: allergi
 
     }
 }
-let Sabrine=classmates("Sabrine",27,"female","Cyan","Reptiles","Smoke")
-let Nassim=classmates("Nassim",25,"Male","Blue","Dogs","Flowers")
-let Ahmed=classmates("Ahmed",26,"Male","Dark Blue","Highlands","Coconut")
+let Sabrine = classmates("Sabrine", 27, "female", "Cyan", "Reptiles", "Smoke")
+let Nassim = classmates("Nassim", 25, "Male", "Blue", "Dogs", "Flowers")
+let Ahmed = classmates("Ahmed", 26, "Male", "Dark Blue", "Highlands", "Coconut")
 
-let classm =[Sabrine,Ahmed,Nassim]
+let classm = [Sabrine, Ahmed, Nassim]
 
-let displayFriend=(classmate)=>{
+let displayFriend = (classmate) => {
     return `${classmate.name},is ${classmate.age} YO ,her/his gender is ${classmate.sexe}, her/his favorite color is ${classmate.favecolor}, she/he does fear of ${classmate.fears},and she/he is allergic to ${classmate.allergi}`
 
 }
 
-let addFriend=(mate)=>{
-classm.push(mate)
-return classm
+let addFriend = (mate) => {
+    classm.push(mate)
+    return classm
 }
-let nbOfMale=(array)=>{
-    array=classm
+let nbOfMale = (array) => {
     let count = 0
-   for (const i of array) {
-    if (i.sexe==="Male")count++
+    for (const e of array) {
+        if (e.sexe === "Male"){
+         count++
+        }
+    }
+    return count
+}
 
-   }
- return count    
+let searchMates = (array, query) => {
+    let output = []
+    for (const e of array) {
+        if (e.name.includes(query)) {
+            output.push(e)
+        }
+    }
+    return output
 }
